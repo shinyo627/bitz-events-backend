3772cf104dc7124ba7545c15ddc711624015791a
shinyo627 <shinyo627@gmail.com>
2021-05-12 17:40:42 +0200

    customized event model on api/events/models/events.js file. Now assigning slug field to name field is automated before  the collection entry is being created and updated

--- api/events/models/events.js
+++ api/events/models/events.js
@@ -1,8 +1,21 @@
-'use strict';
+const slugify = require("slugify");
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+  },
+};
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
-
-module.exports = {};
