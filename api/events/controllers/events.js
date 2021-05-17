bc410844f21d524fa0dff8ad97b2f813a003ba30
shinyo627 <shinyo627@gmail.com>
2021-05-17 21:28:58 +0200

    api/events/controllers/events.js updated with update/delete/create event with linked user

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -7,6 +7,64 @@ const { sanitizeEntity } = require("strapi-utils");
  */
 
 module.exports = {
+  // Create event with linked user
+  async create(context) {
+    let entity;
+    if (context.is("multipart")) {
+      const { data, files } = parseMultipartData(context);
+      data.user = context.state.user.id;
+      entity = await strapi.services.article.create(data, { files });
+    } else {
+      context.request.body.user = context.state.user.id;
+      entity = await strapi.services.article.create(context.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
+  //   Update User event
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      "user.id": ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.events.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.events.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
+  //   Delete a user event
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      "user.id": ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized("You cant update this entry");
+    }
+
+    const entity = await strapi.services.events.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
   // Get logged in users
   async me(context) {
     const user = context.state.user;
