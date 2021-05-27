b643ad6e3c14eb6e781007da31e64565998ba323
shinyo627 <shinyo627@gmail.com>
2021-05-27 12:58:18 +0200

    Updating configuration folder.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7fd227fbf2d0ee8c65548383910bec7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1f6ee684-f405-4a2a-81b1-267c1b4875d2'
+};
